0941a0605b6a73eb2847a264b6de50da66522005
Andres Ruiz <andyruizz_db@hotmail.com>
2023-06-16 00:05:21 -0500

    add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,18 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
+	env: {
+	  node: true,
+	  es2021: true,
 	},
-	'extends': 'eslint:recommended',
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
+	extends: 'eslint:recommended',
+	parserOptions: {
+	  ecmaVersion: 12,
+	  sourceType: 'module',
 	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
-};
+	rules: {
+	
+
+	  'quotes': ['error', 'single'],
+	  
+	},
+  };
+  
