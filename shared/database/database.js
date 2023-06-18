6291dcc5f5fecfddd9674cea7ec9d49c77cce173
Andres Ruiz <andyruizz_db@hotmail.com>
2023-06-18 17:53:30 -0500

    Fix issue database

--- shared/database/database.js
+++ shared/database/database.js
@@ -3,9 +3,15 @@ import { Sequelize } from 'sequelize'
 
 dotenv.config()
 
-const sequelize = new Sequelize('test-db', process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-  dialect: 'sqlite',
-  storage: process.env.DATABASE_NAME
-})
+const databaseName = Buffer.from(process.env.DATABASE_NAME, 'base64').toString('utf-8');
+const databaseUser = Buffer.from(process.env.DATABASE_USER, 'base64').toString('utf-8');
+const databasePassword = Buffer.from(process.env.DATABASE_PASSWORD, 'base64').toString('utf-8');
 
-export default sequelize
+const sequelize = new Sequelize(databaseName, databaseUser, databasePassword, {
+  dialect: 'sqlite',
+  storage: './dev.sqlite'
+});
+
+export default sequelize;
+
+
