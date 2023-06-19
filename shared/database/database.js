99a0e574a7b30622f413ede275ba85ef34f7c3b3
Andres Ruiz <andyruizz_db@hotmail.com>
2023-06-18 21:15:15 -0500

    changes in dabase

--- shared/database/database.js
+++ shared/database/database.js
@@ -3,11 +3,9 @@ import { Sequelize } from 'sequelize'
 
 dotenv.config()
 
-const databaseName = Buffer.from(process.env.DATABASE_NAME, 'base64').toString('utf-8');
-const databaseUser = Buffer.from(process.env.DATABASE_USER, 'base64').toString('utf-8');
-const databasePassword = Buffer.from(process.env.DATABASE_PASSWORD, 'base64').toString('utf-8');
-
-const sequelize = new Sequelize(databaseName, databaseUser, databasePassword, {
+const sequelize = new Sequelize(Buffer.from(process.env.DATABASE_NAME, 'base64').toString('utf-8'),
+                                Buffer.from(process.env.DATABASE_USER, 'base64').toString('utf-8'),  
+                                Buffer.from(process.env.DATABASE_PASSWORD, 'base64').toString('utf-8'), {
   dialect: 'sqlite',
   storage: './dev.sqlite'
 });
